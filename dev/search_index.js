2b41ae7a14d2393192b97aeaebe5d4afc46a4a2d
Documenter.jl <documenter@juliadocs.github.io>
2024-10-15 19:12:16 +0000

    build based on cefd7d4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RecurrentLayers","category":"page"},{"location":"#RecurrentLayers","page":"Home","title":"RecurrentLayers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RecurrentLayers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RecurrentLayers]","category":"page"}]
+}
