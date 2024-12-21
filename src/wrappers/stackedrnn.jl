5c10776ea94a7dc116259176a586615caa4cd922
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2024-12-21 21:26:59 +0100

    adding kwargs to dropout and warning

--- src/wrappers/stackedrnn.jl
+++ src/wrappers/stackedrnn.jl
@@ -7,7 +7,7 @@ end
 
 Flux.@layer StackedRNN trainable=(layers)
 
-"""
+@doc raw"""
     StackedRNN(rlayer, (input_size, hidden_size), args...;
         num_layers = 1, kwargs...)
 
@@ -15,8 +15,8 @@ Constructs a stack of recurrent layers given the recurrent layer type.
 
 Arguments:
   - `rlayer`: Any recurrent layer such as [MGU](@ref), [RHN](@ref), etc... or
-    [Flux.RNN](@extref), [Flux.LSTM](@extref), etc... Additionally anything wrapped in
-    [Flux.recurrence](@extref) can be used as `rlayer`.
+    [`Flux.RNN`](@extref), [`Flux.LSTM`](@extref), etc... Additionally anything wrapped in
+    [`Flux.recurrence`](@extref) can be used as `rlayer`.
   - `input_size`: Defines the input dimension for the first layer.
   - `hidden_size`: defines the dimension of the hidden layer.
   - `num_layers`: The number of layers to stack. Default is 1.
@@ -29,15 +29,26 @@ Returns:
 function StackedRNN(rlayer, (input_size, hidden_size)::Pair, args...;
     num_layers::Int = 1,
     dropout::Number = 0.0,
+    dims = :,
+    active::Union{Bool,Nothing} = nothing,
+    rng = Flux.default_rng(),
     kwargs...)
+    #build container
     layers = []
+    #warn for dropout and num_layers
+    if num_layers ==1 && dropout != 0.0
+        @warn("Dropout is not applied when num_layers is 1.")
+    end
+
     for (idx,layer) in enumerate(num_layers)
         in_size = idx == 1 ? input_size : hidden_size
         push!(layers, rlayer(in_size => hidden_size, args...; kwargs...))
     end
     states = [initialstates(layer) for layer in layers]
 
-    return StackedRNN(layers, Dropout(dropout), states)
+    return StackedRNN(layers,
+        Dropout(dropout; dims = dims, active = active, rng = rng),
+        states)
 end
 
 function (stackedrnn::StackedRNN)(inp::AbstractArray)
