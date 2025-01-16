6f4b23ebd4a831eb4f4683354aeb391154249273
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2025-01-16 13:50:53 +0100

    updated stackedrnn docstrings

--- src/wrappers/stackedrnn.jl
+++ src/wrappers/stackedrnn.jl
@@ -1,4 +1,40 @@
 # based on https://fluxml.ai/Flux.jl/stable/guide/models/recurrence/
+
+@doc raw"""
+    StackedRNN(rlayer, (input_size, hidden_size), args...;
+        num_layers = 1, dropout = 0.0, kwargs...)
+
+Constructs a stack of recurrent layers given the recurrent layer type.
+
+# Arguments:
+  - `rlayer`: Any recurrent layer such as [MGU](@ref), [RHN](@ref), etc... or
+    [`Flux.RNN`](@extref), [`Flux.LSTM`](@extref), etc.
+  - `input_size`: Defines the input dimension for the first layer.
+  - `hidden_size`: defines the dimension of the hidden layer.
+  - `num_layers`: The number of layers to stack. Default is 1.
+  - `dropout`: Value of dropout to apply between recurrent layers.
+    Default is 0.0.
+  - `args...`: Additional positional arguments passed to the recurrent layer.
+
+# Keyword arguments
+  - `kwargs...`: Additional keyword arguments passed to the recurrent layers.
+
+# Examples
+```jldoctest
+julia> stac_rnn = StackedRNN(MGU, (3=>5); num_layers = 4)
+StackedRNN(
+  [
+    MGU(3 => 10),                       # 90 parameters
+    MGU(5 => 10),                       # 110 parameters
+    MGU(5 => 10),                       # 110 parameters
+    MGU(5 => 10),                       # 110 parameters
+  ],
+)         # Total: 12 trainable arrays, 420 parameters,
+          # plus 4 non-trainable, 20 parameters, summarysize 2.711 KiB.
+
+```
+
+"""
 struct StackedRNN{L, D, S}
     layers::L
     dropout::D
@@ -7,24 +43,6 @@ end
 
 @layer StackedRNN trainable=(layers)
 
-@doc raw"""
-    StackedRNN(rlayer, (input_size, hidden_size), args...;
-        num_layers = 1, kwargs...)
-
-Constructs a stack of recurrent layers given the recurrent layer type.
-
-Arguments:
-  - `rlayer`: Any recurrent layer such as [MGU](@ref), [RHN](@ref), etc... or
-    [`Flux.RNN`](@extref), [`Flux.LSTM`](@extref), etc.
-  - `input_size`: Defines the input dimension for the first layer.
-  - `hidden_size`: defines the dimension of the hidden layer.
-  - `num_layers`: The number of layers to stack. Default is 1.
-  - `args...`: Additional positional arguments passed to the recurrent layer.
-  - `kwargs...`: Additional keyword arguments passed to the recurrent layers.
-
-Returns:
-  A `StackedRNN` instance containing the specified number of RNN layers and their initial states.
-"""
 function StackedRNN(rlayer, (input_size, hidden_size)::Pair{<:Int, <:Int}, args...;
         num_layers::Int=1, dropout::Number=0.0, dims=:,
         active::Union{Bool, Nothing}=nothing, rng=default_rng(), kwargs...)
