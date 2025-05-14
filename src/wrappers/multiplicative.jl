07dde228e60d4e85e004904d3c1ef66c91da238a
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2025-05-14 09:39:37 +0200

    docs: add docstrings for Multiplicative

--- src/wrappers/multiplicative.jl
+++ src/wrappers/multiplicative.jl
@@ -14,12 +14,69 @@ a given `cell`, and performs the following forward pass
 
 ## Arguments
 
+  - `rcell`: A recurrent cell constructor such as [MGUCell](@ref), or
+   [`Flux.LSTMCell`](@extref) etc.
+  - `input_size`: Defines the input dimension for the first layer.
+  - `hidden_size`: defines the dimension of the hidden layer.
+  - `args...`: positional arguments for the `rcell`.
+
 ## Keyword arguments
+
+  - `init_multiplicative_kernel`:Initializer for the multiplicative input kernel.
+    Default is glorot_uniform.
+  - `init_multiplicativerecurrent_kernel`:Initializer for the multiplicative recurrent
+    kernel. Default is glorot_uniform.
+  - `kwargs...`: keyword arguments for the `rcell`.
+
+# Forward
+
+    mrnn(inp, state)
+    mrnn(inp, (state, c_state))
+    mrnn(inp)
+
+## Arguments
+
+  - `inp`: The input to the `rcell`. It should be a vector of size `input_size`
+   or a matrix of size `input_size x batch_size`.
+  - `state`: The hidden state of the `rcell`, is single return.
+    It should be a vector of size `hidden_size` or a matrix of size
+    `hidden_size x batch_size`. If not provided, it is assumed to be a
+    vector of zeros, initialized by [`Flux.initialstates`](@extref).
+  - `(state, cstate)`: A tuple containing the hidden and cell states of the `rcell`.
+    if double return. They should be vectors of size `hidden_size` or matrices of size
+    `hidden_size x batch_size`. If not provided, they are assumed to be vectors of zeros,
+    initialized by [`Flux.initialstates`](@extref).
+
+## Returns
+
+Either of 
+  - A tuple `(output, state)`, where both elements are given by the updated state
+    `new_state`, a tensor of size `hidden_size` or `hidden_size x batch_size`, if the
+    `rcell` is single return (e.g. [`Flux.RNNCell`](@extref)).
+
+  - A tuple `(output, state)`, where `output = new_state` is the new hidden state and
+    `state = (new_state, new_cstate)` is the new hidden and cell state. 
+    They are tensors of size `hidden_size` or `hidden_size x batch_size`.
+    This applies if the `rcell` is double return (e.g. [`Flux.LSTMCell`](@extref)).
+
+# Examples
+
+```jldoctest
+julia> using RecurrentLayers
+
+julia> mrnn = Multiplicative(MGUCell, 3 => 5)
+Multiplicative(
+  5×3 Matrix{Float32},                  # 15 parameters
+  5×5 Matrix{Float32},                  # 25 parameters
+  MGUCell(3 => 5),                      # 90 parameters
+)                   # Total: 5 arrays, 130 parameters, 792 bytes.
+
+```
 """
-struct Multiplicative{C, M, H}
-    cell::C
+struct Multiplicative{M, H, C}
     Wm::M
     Wh::H
+    cell::C
 end
 
 @layer Multiplicative
@@ -34,7 +91,7 @@ function Multiplicative(rcell, (input_size, hidden_size)::Pair{<:Int, <:Int}, ar
     cell = rcell(input_size => hidden_size, args...; kwargs...)
     Wm = init_multiplicative_kernel(hidden_size, input_size)
     Wh = init_multiplicativerecurrent_kernel(hidden_size, hidden_size)
-    return Multiplicative(cell, Wm, Wh)
+    return Multiplicative(Wm, Wh, cell)
 end
 
 function (mrnn::Multiplicative)(inp::AbstractVecOrMat, state::AbstractVecOrMat)
@@ -44,7 +101,7 @@ function (mrnn::Multiplicative)(inp::AbstractVecOrMat, state::AbstractVecOrMat)
 end
 
 function (mrnn::Multiplicative)(inp::AbstractVecOrMat, (state, c_state))
-    m_state = (mrnn.WM * inp) .* (mrnn.Wh * state)
+    m_state = (mrnn.Wm * inp) .* (mrnn.Wh * state)
     new_state = mrnn.cell(inp, (m_state, c_state))
     return new_state, (new_state, c_state)
 end
