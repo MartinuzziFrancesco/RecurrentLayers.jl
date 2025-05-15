6ae719af14dd2e18f1fd228a7cf6f305b134feff
MartinuzziFrancesco <martinuzzi.francesco@gmail.com>
2025-05-15 10:16:39 +0200

    fix: correct peepholelstmcell forward pass

--- src/cells/peepholelstm_cell.jl
+++ src/cells/peepholelstm_cell.jl
@@ -3,6 +3,7 @@
     PeepholeLSTMCell(input_size => hidden_size;
         init_kernel = glorot_uniform,
         init_recurrent_kernel = glorot_uniform,
+        init_peephole_kernel = glorot_uniform,
         bias = true)
 
 [Peephole long short term memory cell](https://www.jmlr.org/papers/volume3/gers02a/gers02a.pdf).
@@ -18,17 +19,20 @@ See [`PeepholeLSTM`](@ref) for a layer that processes entire sequences.
     Default is `glorot_uniform`.
 - `init_recurrent_kernel`: initializer for the hidden to hidden weights.
     Default is `glorot_uniform`.
+- `init_peephole_kernel`: initializer for the hidden to peephole weights.
+    Default is `glorot_uniform`.
 - `bias`: include a bias or not. Default is `true`.
 
 # Equations
 
 ```math
 \begin{aligned}
-f_t &= \sigma_g(W_f x_t + U_f c_{t-1} + b_f), \\
-i_t &= \sigma_g(W_i x_t + U_i c_{t-1} + b_i), \\
-o_t &= \sigma_g(W_o x_t + U_o c_{t-1} + b_o), \\
-c_t &= f_t \odot c_{t-1} + i_t \odot \sigma_c(W_c x_t + b_c), \\
-h_t &= o_t \odot \sigma_h(c_t).
+    z_t &= \tanh(W_z x_t + U_z h_{t-1} + b_z), \\
+    i_t &= \sigma(W_i x_t + U_i h_{t-1} + p_i \odot c_{t-1} + b_i), \\
+    f_t &= \sigma(W_f x_t + U_f h_{t-1} + p_f \odot c_{t-1} + b_f), \\
+    c_t &= f_t \odot c_{t-1} + i_t \odot z_t, \\
+    o_t &= \sigma(W_o x_t + U_o h_{t-1} + p_o \odot c_t + b_o), \\
+    h_t &= o_t \odot \tanh(c_t).
 \end{aligned}
 ```
 
@@ -51,9 +55,10 @@ h_t &= o_t \odot \sigma_h(c_t).
   `state = (new_state, new_cstate)` is the new hidden and cell state. 
   They are tensors of size `hidden_size` or `hidden_size x batch_size`.
 """
-struct PeepholeLSTMCell{I, H, V} <: AbstractDoubleRecurrentCell
+struct PeepholeLSTMCell{I, H, P, V} <: AbstractDoubleRecurrentCell
     Wi::I
     Wh::H
+    Wp::P
     bias::V
 end
 
@@ -61,20 +66,23 @@ end
 
 function PeepholeLSTMCell((input_size, hidden_size)::Pair{<:Int, <:Int};
         init_kernel=glorot_uniform, init_recurrent_kernel=glorot_uniform,
-        bias::Bool=true)
+        init_peephole_kernel=glorot_uniform, bias::Bool=true)
     Wi = init_kernel(hidden_size * 4, input_size)
     Wh = init_recurrent_kernel(hidden_size * 4, hidden_size)
+    Wp = init_peephole_kernel(hidden_size * 3)
     b = create_bias(Wi, bias, hidden_size * 4)
-    return PeepholeLSTMCell(Wi, Wh, b)
+    return PeepholeLSTMCell(Wi, Wh, vec(Wp), b)
 end
 
 function (lstm::PeepholeLSTMCell)(inp::AbstractVecOrMat, (state, c_state))
     _size_check(lstm, inp, 1 => size(lstm.Wi, 2))
     b = lstm.bias
-    g = lstm.Wi * inp .+ lstm.Wh * c_state .+ b
-    input, forget, cell, output = chunk(g, 4; dims=1)
-    new_cstate = @. sigmoid_fast(forget) * c_state + sigmoid_fast(input) * tanh_fast(cell)
-    new_state = @. sigmoid_fast(output) * tanh_fast(new_cstate)
+    gates = lstm.Wi * inp .+ lstm.Wh * state .+ b
+    input, forget, cell, output = chunk(gates, 4; dims=1)
+    gpeep = chunk(lstm.Wp, 3; dims=1)
+    new_cstate = @. sigmoid_fast(forget + gpeep[1] * c_state) * c_state +
+                    sigmoid_fast(input + gpeep[2] * c_state) * tanh_fast(cell)
+    new_state = @. sigmoid_fast(output + gpeep[3] * c_state) * tanh_fast(new_cstate)
     return new_state, (new_state, new_cstate)
 end
 
